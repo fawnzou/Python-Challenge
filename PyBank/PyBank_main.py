d3e3f7ddd68f4e28c709e0171e04f47234e9c121
Fawnzou <40528918+fawnzou@users.noreply.github.com>
2019-03-02 18:11:50 -0600

    Update PyBank_main.py

--- PyBank/PyBank_main.py
+++ PyBank/PyBank_main.py
@@ -7,6 +7,7 @@ budget_data_csv_path = os.path.join("..", "Resources", "budget_data.csv")
 with open(budget_data_csv_path, newline="", encoding='utf-8-sig') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
     total_months = 0
+    total_amount = 0
     prev_amount=0
     cur_amount=0
     change_amount=0
@@ -22,29 +23,29 @@ with open(budget_data_csv_path, newline="", encoding='utf-8-sig') as csvfile:
         total_months+=1
         if total_months ==2:
             # Set the value of 'Profit/Losses' in the first row as previous amount
-            # convert value of 'Profit/Losses' from string to float
-            prev_amount=float(row[1])
+            # convert value of 'Profit/Losses' from string to integer
+            prev_amount=int(row[1])
             # add value to total_amount
-            total_amount+=float(row[1])
+            total_amount+=int(row[1])
         elif total_months >2:
             # add value to total_amount
-            total_amount+=float(row[1])
-            # Keep track change amount, and put it into dictionary as key,put its date into
+            total_amount+=int(row[1])
+            # Keep track change amount, and store it into dictionary as key,store its date into
             # dictionary as key
-            cur_amount=float(row[1])
+            cur_amount=int(row[1])
             change_amount=cur_amount-prev_amount
             change_amount_dic[change_amount]=row[0]
             prev_amount=cur_amount
 
 greatest_increase = max(change_amount_dic)
 greatest_increase_month=change_amount_dic[greatest_increase]
-greatest_increase=int(greatest_increase)
+
 greatest_decrease = min(change_amount_dic)
 greatest_decrease_month=change_amount_dic[greatest_decrease]
-greatest_decrease=int(greatest_decrease)
+
 ave_change = round((sum(change_amount_dic)/len(change_amount_dic)),2)
 
-#Output result
+#output result
 print ("Financial Analysis") 
 print ('-'*30)
 print (f'Total Months: {total_months-1}')
@@ -53,5 +54,3 @@ print(f'Average Change: ${ave_change}')
 print (f'Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})')
 print (f'Greatest decrease in Profits: {greatest_decrease_month} (${greatest_decrease})')
        
-
-
